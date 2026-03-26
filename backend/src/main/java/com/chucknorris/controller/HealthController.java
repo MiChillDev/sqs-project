e06203970dfc18e68a296b03062d0d9a8a429f25
MichiN311 <93664963+MiChillDev@users.noreply.github.com>
2026-03-26 16:54:59 +0100

    chore: move path for app code

--- backend/src/main/java/com/chucknorris/controller/HealthController.java
+++ backend/src/main/java/com/chucknorris/controller/HealthController.java
@@ -1,4 +1,4 @@
-package com.michilldev.controller;
+package com.chucknorris.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
--- backend/src/test/java/com/chucknorris/ChuckNorrisApplicationTests.java
+++ backend/src/test/java/com/chucknorris/ChuckNorrisApplicationTests.java
@@ -1,4 +1,4 @@
-package com.michilldev;
+package com.chucknorris;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
