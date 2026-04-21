6aac53394028a65a77bebc6afce1a46c8dea9f8e
studgeisni5338 <nico.geis@stud.th-rosenheim.de>
2026-04-21 11:47:33 +0200

    #18: flyway integration

--- backend/src/main/java/com/chucknorris/jokes/models/repository/JokeRepository.java
+++ backend/src/main/java/com/chucknorris/jokes/models/repository/JokeRepository.java
@@ -1,5 +1,6 @@
 package com.chucknorris.jokes.models.repository;
 
+import com.chucknorris.jokes.models.entity.JokeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
