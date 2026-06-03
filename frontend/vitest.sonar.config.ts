b0206c73ff4005fb3cd7d5f1174119fdc90fdb64
Nico <270573724+nicomi24@users.noreply.github.com>
2026-06-03 16:14:52 +0200

    fix frontend coverage (#62)
    * setup(#47) export coverage to sonarcloud backend and frontend
    
    * fix(#47): workflow for frontend and backend
    
    * refactor(#47) biome check
    
    * fix(#47) remove verify gate from sonarcloud workflow
    
    * debug(#47): show generated files in log
    
    * bugfix(#47): show mor logs
    
    * bugfix(#47): change logs
    
    * fix(#47): repair test command for frontend
    
    * fix(#47): use test instead of vitest
    
    * fix(#47): add new vitest config file for sonar that does not stop when threshold is not reached

---
+++ frontend/vitest.sonar.config.ts
@@ -0,0 +1,20 @@
+import path from 'node:path';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./tests/setup.ts'],
+    include: ['tests/unit/**/*.test.{ts,tsx}', 'src/**/__tests__/**/*.test.{ts,tsx}'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html', ['lcov', { projectRoot: '../' }]],
+    },
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, './src'),
+    },
+  },
+});
