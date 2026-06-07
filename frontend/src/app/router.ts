3ae6b7c4fa4518baf94a71fa4780f68587b429c7
stopwatch571 <271630759+stopwatch571@users.noreply.github.com>
2026-06-07 21:40:41 +0200

    feat: remove component demo pages

--- frontend/src/app/router.ts
+++ frontend/src/app/router.ts
@@ -2,22 +2,11 @@ import { createRouter } from '@tanstack/react-router';
 
 import { rootRoute } from './routes/__root';
 import adminRoute from './routes/admin';
-import componentTestRoute from './routes/component-demo';
-import demoApiRoute from './routes/demo-api';
 import indexRoute from './routes/index';
 import jokePageRoute from './routes/joke-page';
 import loginRoute from './routes/login';
-import referenceRoute from './routes/reference';
 
-const routeTree = rootRoute.addChildren([
-  indexRoute,
-  componentTestRoute,
-  referenceRoute,
-  demoApiRoute,
-  jokePageRoute,
-  loginRoute,
-  adminRoute,
-]);
+const routeTree = rootRoute.addChildren([indexRoute, jokePageRoute, loginRoute, adminRoute]);
 
 export const router = createRouter({ routeTree });
 
